cefad02b48e7d70aed89c642ddd1de3ad1d3e29a
pratikregal1a <57472214+pratikregal1a@users.noreply.github.com>
2021-11-25 17:00:47 +0530

    Update invoke.go

--- invoke.go
+++ invoke.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"image"
+	"image/png"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,6 +26,11 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+	}
+	{
+      image1,err := os.Open("**/*.png")
+	if err != nil {
+		log.Fatalf("failed to open: %s", err)
 	}
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
